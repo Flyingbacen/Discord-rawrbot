a0996f3c5a948c20a82c4f141520905a0acf052d
aster <thisgacha@gmail.com>
2024-05-21 20:17:41 -0400

    remove unused code

--- Discord_bot-SLASH.py
+++ Discord_bot-SLASH.py
@@ -167,27 +167,6 @@ async def CueRedeem(interaction: discord.Interaction, userid: int):
 				await interaction.followup.send(f"Failed to retrieve SKU for the free cue piece with category wildcard: {category_wildcard}.")
 
 
-# Thank you discord.py examples :3
-class Confirm(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.value = None
-    # When the confirm button is pressed, set the inner value to `True` and
-    # stop the View from listening to more input.
-    # We also send the user an ephemeral message that we're confirming their choice.
-    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.success)
-    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Confirming', ephemeral=True)
-        self.value = True
-        self.stop()
-
-    # This one is similar to the confirmation button except sets the inner value to `False`
-    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.danger)
-    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Cancelling', ephemeral=True)
-        self.value = False
-        self.stop()
-
 @tree.context_menu(name="message")
 async def dm(interaction: discord.Interaction, message: discord.Message):
 	"""I should probably remove this lmao"""
